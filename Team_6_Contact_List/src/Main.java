66d48c83324ee14cda48f8ce99215ddd677198b7
Tony Nam Tran <tonyvalkyrie@gmail.com>
2012-11-11 11:51:44 -0800

    Added my twin brother for testing purposes.

--- Team_6_Contact_List/src/Main.java
+++ Team_6_Contact_List/src/Main.java
@@ -5,8 +5,9 @@ public class Main {
 		System.out.println("Hello");
 		
 		
-		System.out.println("This edit was made by Tony.");
+		System.out.println("This edit was made by Tony."); //by Tony
 		System.out.println("This edit was made by Johnna."); //by Johnna
+		System.out.println("This edit was made by Tony #2, his evil twin."); //by Tony for testing purposes
 		
 		
 	}
