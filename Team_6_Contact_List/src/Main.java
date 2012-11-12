b06adcc9c2500084282b56b9109e145bc9f28394
Johnna Benson <johnnabcornett@gmail.com>
2012-11-11 19:44:45 -0800

    Reinstating evil twin's line

--- Team_6_Contact_List/src/Main.java
+++ Team_6_Contact_List/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		
 		System.out.println("This edit was made by Tony."); //by Tony
 		System.out.println("This edit was made by Johnna."); //by Johnna
-		//System.out.println("This edit was made by Tony #2, his evil twin."); //by Tony for testing purposes
+		System.out.println("This edit was made by Tony #2, his evil twin."); //by Tony for testing purposes
 		
 		
 	}
